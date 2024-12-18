bdbee7a5eb78d6e12c14c991323ac9a1ad8b693b
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:43 +0000

    xUnit v3

--- test/Soenneker.Blazor.TomSelect.Tests/Fixture.cs
+++ test/Soenneker.Blazor.TomSelect.Tests/Fixture.cs
@@ -8,7 +8,7 @@ namespace Soenneker.Blazor.TomSelect.Tests;
 
 public class Fixture : UnitFixture
 {
-    public override System.Threading.Tasks.Task InitializeAsync()
+    public override System.Threading.Tasks.ValueTask InitializeAsync()
     {
         SetupIoC(Services);
 
--- test/Soenneker.Blazor.TomSelect.Tests/TomSelectTests.cs
+++ test/Soenneker.Blazor.TomSelect.Tests/TomSelectTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.Blazor.TomSelect.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.Blazor.TomSelect.Tests;
 

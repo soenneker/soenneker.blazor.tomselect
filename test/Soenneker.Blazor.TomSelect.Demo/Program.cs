ffc2d887a19e3f4fec6ac61c29714d3b5e8e7729
Jake Soenneker <jake@soenneker.com>
2026-04-03 20:48:52 -0500

    quark gen

--- test/Soenneker.Blazor.TomSelect.Demo/Program.cs
+++ test/Soenneker.Blazor.TomSelect.Demo/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.JSInterop;
 using Serilog;
 using Serilog.Debugging;
 using Soenneker.Blazor.TomSelect.Registrars;
+using Soenneker.Quark.Gen.Lucide.Generated;
 using Soenneker.Serilog.Sinks.Browser.Blazor.Registrars;
 
 namespace Soenneker.Blazor.TomSelect.Demo;
@@ -34,6 +35,7 @@ public class Program
 
             builder.Services.AddTomSelectInteropAsScoped();
             builder.Services.AddQuarkSuiteAsScoped();
+            builder.Services.AddLucideIconsAsScoped();
 
             WebAssemblyHost host = builder.Build();
 
